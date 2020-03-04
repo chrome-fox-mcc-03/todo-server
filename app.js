94603b97309cbc62af5498c8725010b5198cd016
prmzk <adam.primarizki@gmail.com>
2020-03-04 11:22:45 +0700

    add cors to pkg

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const router = require('./routes/index')
 
-
+app.use(cors())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json())
 app.use(router)
