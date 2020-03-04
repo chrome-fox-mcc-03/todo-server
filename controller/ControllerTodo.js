703a16b0bde2cb62db6507482a10aa72fc53ee21
adam jay <adamjay041@gmail.com>
2020-03-04 13:59:17 +0700

    note controller todo

--- controller/ControllerTodo.js
+++ controller/ControllerTodo.js
@@ -51,56 +51,6 @@ class ControllerTodo {
           next(err)
 				})
     }
-    static addTodoTeam (req, res, next) {
-		const {title, description, due_date} = req.body
-		Todo.create({
-      title,
-      description,
-      due_date,
-      ProjectTeamId: id
-		})
-			.then(data => {
-        res.status(201).json(data)
-			})
-			.catch(err => {
-        next(err)
-			})
-    }
-    static updateTodoTeam (req, res, next) {
-    const { status } = req.body
-    const { id } = req.params
-      Todo.findOne({
-        where: {
-          ProjectTeamId: teamId
-        }
-      })
-        .then(data => {
-          if(data) {
-            return Todo.update(
-              {
-                status
-              },
-              {
-                where: {
-                  id
-                }
-              }
-          )}
-          else{
-            throw {
-              name: "costume",
-              status: 404,
-              message: "Project is undifiend"
-            }
-          }
-        })
-        .then(data => {
-          res.status(200).json(data)
-        })
-        .catch(err => {
-          next(err)
-        })
-    }
     static updateTodoUser (req, res, next) {
 			const { status } = req.body
 			const { id } = req.params
@@ -138,30 +88,6 @@ class ControllerTodo {
             next(err)
 					})
     }
-    static deleteTodoTeam (req, res, next) {
-        const { id } = req.params
-        Todo.destroy(
-              {
-                where: {
-                  ProjectTeamId
-                }
-              }
-            )
-            .then(data => {
-              if(data) {
-                res.status(200).json({note: 'delete succses'})
-              }else {
-                next({
-                  name: "costume",
-                  status: 404,
-                  message: "TodoUser not Found"
-                })
-              }
-            })
-            .catch(err => {
-              next(err)
-            })
-    }
     static deleteTodoUser (req, res, next) {
     const { id } = req.params
       Todo.findOne(
@@ -193,6 +119,81 @@ class ControllerTodo {
           next(err)
         })
     }
+    //Team belum jalan
+    static deleteTodoTeam (req, res, next) {
+      const { id } = req.params
+      Todo.destroy(
+            {
+              where: {
+                ProjectTeamId
+              }
+            }
+          )
+          .then(data => {
+            if(data) {
+              res.status(200).json({note: 'delete succses'})
+            }else {
+              next({
+                name: "costume",
+                status: 404,
+                message: "TodoUser not Found"
+              })
+            }
+          })
+          .catch(err => {
+            next(err)
+          })
+    }
+    static updateTodoTeam (req, res, next) {
+      const { status } = req.body
+      const { id } = req.params
+        Todo.findOne({
+          where: {
+            ProjectTeamId: teamId
+          }
+        })
+          .then(data => {
+            if(data) {
+              return Todo.update(
+                {
+                  status
+                },
+                {
+                  where: {
+                    id
+                  }
+                }
+            )}
+            else{
+              throw {
+                name: "costume",
+                status: 404,
+                message: "Project is undifiend"
+              }
+            }
+          })
+          .then(data => {
+            res.status(200).json(data)
+          })
+          .catch(err => {
+            next(err)
+          })
+    } 
+    static addTodoTeam (req, res, next) {
+      const {title, description, due_date} = req.body
+      Todo.create({
+        title,
+        description,
+        due_date,
+        ProjectTeamId: id
+      })
+        .then(data => {
+          res.status(201).json(data)
+        })
+        .catch(err => {
+          next(err)
+        })
+      }
 }
 
 module.exports = ControllerTodo
