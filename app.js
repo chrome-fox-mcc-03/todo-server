c439e8eac31ba7f63bd6e9e4d0ee3cd1b2095ac1
Juli Yanti <juleshwa@gmail.com>
2020-03-04 14:51:48 +0700

    Add Cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ if (process.env.NODE_ENV === 'development') require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT;
 const { router } = require('./router/router');
 const { ErrorHandler } = require('./middlewares/errorHandler');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
