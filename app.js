4faf158473b9c90b8417f6b2e873afe05097dc4c
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-04 09:14:27 +0700

    adding themoviedb api

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const express = require('express')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
+const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(router)
 app.use(errorHandler)
 
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}!`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
