61c6ebcbb8c0d339a7a1c7c4d309430c27e94475
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-06 01:42:44 +0700

    add google login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,8 @@
 const { User } = require('../models')
 const { decryptPassword } = require('../helpers/bcrypt')
 const { generateToken } = require('../helpers/jwt')
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
 
 class UserController {
     static register(req, res, next) {
@@ -55,6 +57,67 @@ class UserController {
             })
             .catch(err => {
                 next(err)
+            })
+    }
+
+    static googleLogin(req, res, next) {
+        console.log(req.body.id_token)
+        const { id_token } = req.body
+        client
+            .verifyIdToken({
+                idToken: id_token,
+                audience: process.env.CLIENT_ID
+            })
+            .then(result => {
+                console.log(result.payload)
+                const { email } = result.payload
+                // cek dah ada pa belom, kalo udah ada
+                User.findOne({ where: { email } })
+                    .then(user => {
+                        if (user) {
+                            const isGoogleAuth = decryptPassword(
+                                process.env.GOOGLE_PASSWORD,
+                                user.password
+                            )
+                                // sebelumnya sudah register menggunakan google auth
+                            if (isGoogleAuth) {
+                                // generate token terus login berhasil
+                                const payload = {
+                                    id: user.id,
+                                    email: user.email
+                                }
+                                const message = 'Successfully logged in.'
+                                const token = generateToken(payload)
+                                res.status(200).json({ token, message })
+                            } else {
+                                res.status(400).json({
+                                    message: 'Email sudah terdaftar'
+                                })
+                            }
+                        } else {
+                            // kita register disini
+                            const newUser = {
+                                email,
+                                password: process.env.GOOGLE_PASSWORD
+                            }
+
+                            User.create(newUser)
+                                .then(result => {
+                                    const payload = {
+                                        id: result.id,
+                                        email: result.email
+                                    }
+                                    const message = 'Successfully logged in.'
+                                    const token = generateToken(payload)
+                                    res.status(200).json({ token, message })
+                                })
+                                .catch(next)
+                        }
+                    })
+                    .catch(next)
+            })
+            .catch(err => {
+                console.log(err)
             })
     }
 }
