c56244124f824b6e21a84e595337eca05396e1b2
felixwinangun <felixwinangun@ymail.com>
2020-03-05 09:08:43 +0700

    add google sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const { User } = require('../models');
 const jwt = require("../helpers/jwt");
 const ErrorModel = require('../helpers/error');
 const bcrypt = require('../helpers/bcrypt');
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 class Controller {
     static login(req, res, next) {
@@ -35,6 +37,41 @@ class Controller {
                 const payload = jwt.generateToken({ id: result.id });
                 res.status(201).json({ token: payload });
             }).catch(next);
+    }
+
+    static googleLogin(req, res, next) {
+        let gtoken = req.headers.gtoken
+        let payload;
+
+        client.verifyIdToken({
+            idToken: gtoken,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+        .then((result) => {
+            payload = result.getPayload();
+            console.log(payload);
+            return User.findOne({
+                where: {
+                    email: payload.email
+                },
+            })
+        })
+        .then((result) => {
+            if(result) {
+                return { id: result.id };
+            } else {
+                return User.create({
+                    email: payload.email,
+                    password: payload.email + "g"
+                })
+            }
+        })
+        .then((result) => {
+            console.log(result); 
+            const payload = jwt.generateToken({ id: result.id })
+            res.status(200).json({ token: payload })
+        })
+        .catch(next);
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.use("/todos", todoRoutes);
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
+router.post("/googleLogin", UserController.googleLogin);
 
 module.exports = router;
