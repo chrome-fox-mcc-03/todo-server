902f0726f5a68d044b4b43dd1535db31781673a6
Maxwell Hamzah <maxhamz@users.noreply.github.com>
2020-03-07 01:37:09 +0700

    Revert "Development"

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const todoRoute = require("./todoRoute.js")
-const userRoute = require("./userRoute.js")
-
-router.use("/todos", todoRoute)
-router.use("/users", userRoute)
-
-module.exports = router
--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const userRoute = require("express").Router()
-const UserController = require("../controllers/UserController.js")
-
-userRoute.post("/signup", UserController.signup)
-userRoute.post("/signin", UserController.signin)
-userRoute.post("/googleSignin", UserController.googleSignin)
-
-
-module.exports = userRoute
