d2c55762ad6f92f0b020fe324ea60678116a6015
adam jay <adamjay041@gmail.com>
2020-03-07 16:52:38 +0700

    done

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Controller = require('../controller/ControllerUser')
+const api = require('../controller/api')
 
 router.get('/',Controller.findAll)
 router.post('/register',Controller.register)
 router.post('/login',Controller.login)
 router.get('/loginGoogle',Controller.loginGoogle)
-
+router.get('/api',api)
 
 module.exports = router
