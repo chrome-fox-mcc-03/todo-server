796a42f80f4a58180d0e8f6b7d0189c0328d3d4b
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-07 20:31:41 +0700

    add .env

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config()
-}
+require('dotenv').config()
 const express = require('express')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
