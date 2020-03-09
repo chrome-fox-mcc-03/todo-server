fccc14db271c23ba29102804a236aa228caa3a3e
andreasanggaraa <andreasanggaraa@gmail.com>
2020-03-09 14:46:38 +0700

    changes from laptop

--- Routes/index.js
+++ Routes/index.js
@@ -3,9 +3,6 @@ const todoRouters = require('../Routes/TodoRouters.js');
 const userRouters = require('../Routes/UserRouters.js');
 const UserController = require('../Controllers/UserController.js')
 
-
-
-
 router.use('/register', UserController.register);
 router.use('/login', UserController.login);
 router.post('/googleLogIn', UserController.googleLogIn);
