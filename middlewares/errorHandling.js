93f98a29456e56be52a0c96bab8827e5eaa1c103
felixwinangun <felixwinangun@ymail.com>
2020-03-09 19:05:18 +0700

    remove win-env

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -2,9 +2,9 @@ const errorHandling = (err, req, res, next) => {
     console.log(err);
     if (err.name == "SequelizeValidationError") {
         let error = err.errors.map(el => el.message);
-        res.status(400).json(error[0]);
+        res.status(400).json({ error: error[0] });
     } else if (err.name == "JsonWebTokenError") { //auth error
-        res.status(401).json("Please login first!");
+        res.status(401).json({ error: err.message });
     } else {
         res.status(err.status || 500).json({ error: err.message } || { error: "Internal Server Error" } );
     }
