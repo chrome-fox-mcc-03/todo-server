df8769bbfa4bac4fca208a1fd07ee7853422c3c2
thejatmik <jatmikatejasukmana@gmail.com>
2020-03-09 14:43:51 +0700

    heroku config

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const todoRouter = require('./todo');
 
 const UserController = require('../controllers/UserController')
 
+router.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Database online"
+    })
+})
+
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/gAuth", UserController.gAuth);
