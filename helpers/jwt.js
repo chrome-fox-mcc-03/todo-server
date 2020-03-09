ed300b026ff25a55b19b95e55f6a389627ae5d83
maxhamz <maxwell.hamzah@gmail.com>
2020-03-09 17:43:09 +0700

    tried to limit token validity to 1 hour

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 function generateToken(thing) {
-    return jwt.sign(thing, process.env.SECRET)
+    return jwt.sign(thing, process.env.SECRET, {
+        expiresIn: '1h'
+    })
 }
 
 function verifyToken(token) {
-    return jwt.verify(token, process.env.SECRET)
+    return jwt.verify(token, process.env.SECRET, {
+        maxAge: "1h"
+    })
 }
 
 module.exports = {
