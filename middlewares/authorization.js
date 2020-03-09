6ea9ae86213ef649febe393534fb09c52ea5ac1b
MickeyStrike <irfanmaulana281299@gmail.com>
2020-03-09 15:51:15 +0700

    fixed bug

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,28 +3,26 @@ const {Todo} = require('../models')
 
 module.exports = {
     authorization: function(req,res,next){
-        let {id} = req.params
-        let {token} = req.headers
-        let decoded = jwt.verify(token,process.env.secretKey)
-        try{
-            Todo.findOne({
-                where:{
-                    id
-                }
-            })
-            .then((result) => {
-                if(result.dataValues.UserId == decoded.id) {
-                    next()
-                }else{
-                    next({status:401,message:'Not Authorized'})
-                }
-            })
-            .catch((err) => {
+    let {id} = req.params
+    let {token} = req.headers
+    let decoded = jwt.verify(token,process.env.secretKey)
+        Todo.findOne({
+            where:{
+                id
+            }
+        })
+        .then((result) => {
+            if(result.dataValues.UserId == decoded.id) {
+                next()
+            }else{
                 next({status:401,message:'Not Authorized'})
-            })
-        }
-        catch(err){
+            }
+        })
+        .catch((err) => {
             next({status:401,message:'Not Authorized'})
-        }
+        })
     }
+    .catch((err) => {
+        next({status:401,message:'Not Authorized'})
+    })
 }
