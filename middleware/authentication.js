3d161ba9d0b2f0e8269d8d6c1667017889ac8804
naufalyunan <naufalyunan45@gmail.com>
2020-03-10 21:39:10 +0700

    revisi authenticate

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,11 +1,28 @@
+const { user } = require('./../models')
 const checkAuthent = require('./../helper/jwt').checkAuthent
 function authenticate(req,res,next){
     try {
         const decoded = checkAuthent(req.headers.token)
-        console.log(decoded)
-        req.decoded = decoded
-        console.log(req.decoded)
-        next()
+        //perlu di findone
+        const id = decoded.id
+        user.findOne({
+            where: {
+                id
+            }
+        })
+            .then(result => {
+                if (!result) {
+                    throw ({name:"noIdFound" ,errors: [{message: "Id not found"}]})
+                } else {
+                    req.decoded = result
+                    console.log(decoded)
+                    console.log(req.decoded)
+                    next()
+                }
+            })
+            .catch(err => {
+                next(err)
+            })
     } catch(err) {
         next(err)
     }
